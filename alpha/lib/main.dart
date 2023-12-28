8fe539efd692a41e6d95690fd3f5f2c4b1b6d213
AneeqaWaheed <aneeqawaheed19@gmail.com>
2023-12-29 01:14:45 +0500

    Login Screen

--- alpha/lib/main.dart
+++ alpha/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'Screens/auth/login_screen.dart';
 import 'screens/home_screen.dart';
 
+//global object for accessing device screen size
+late Size mq;
 void main() {
   runApp(const MyApp());
 }
@@ -14,16 +17,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Alpha',
         theme: ThemeData(
-          appBarTheme: AppBarTheme(
-            centerTitle: true,
-            elevation: 1,
-            iconTheme: IconThemeData(color: Colors.white),
-            titleTextStyle: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 19),
-            backgroundColor: Color(0xFF7E22CE),
-          ),
-          useMaterial3: true,
-        ),
-        home: const HomeScreen());
+            // appBarTheme: AppBarTheme(
+            //   centerTitle: true,
+            //   elevation: 1,
+            //   iconTheme: IconThemeData(color: Colors.white),
+            //   titleTextStyle: TextStyle(
+            //       color: Colors.white, fontWeight: FontWeight.bold, fontSize: 19),
+            //   backgroundColor: Color(0xFF7E22CE),
+            // ),
+            // useMaterial3: true,
+            ),
+        home: const LoginScreen());
   }
 }
