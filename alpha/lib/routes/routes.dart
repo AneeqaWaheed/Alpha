c76bba4a9900dd75a5145346c78579b93b976de5
AneeqaWaheed <aneeqawaheed19@gmail.com>
2024-01-06 19:11:57 +0500

    Routing

--- alpha/lib/routes/routes.dart
+++ alpha/lib/routes/routes.dart
@@ -1,1 +1,16 @@
+import 'package:alpha/screens/home_screen.dart';
+import 'package:alpha/Screens/auth/login_screen.dart';
+import 'package:get/get.dart';
 
+class RoutesClass {
+  static String Login = "/";
+  static String Home = "/Home";
+
+  static String getLoginRoute() => Login;
+  static String getHomeRoute() => Home;
+
+  static List<GetPage> routes = [
+    GetPage(page: () => LoginScreen(), name: Login),
+    GetPage(page: () => HomeScreen(), name: Home),
+  ];
+}
